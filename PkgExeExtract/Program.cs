8c77e4d711dd1371b25560c103bceedfa32fb42c
zhenxin0603 <zhenxin0603@qq.com>
2019-11-18 15:56:57 +0800

    完成从pkg 打包后的 exe 中提取文件

---
+++ PkgExeExtract/Program.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PkgExeExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ExeFileName = @"D:\TelpoFaceServer\TelpoFaceServer.exe";
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(@"[^']*'([\d]+)[ ]*'[^']*");
+            using (StreamReader br = new StreamReader(ExeFileName, Encoding.ASCII, false))
+            {
+                var tmp = br.BaseStream.ScanAOB(Encoding.ASCII.GetBytes("PAYLOAD_POSITION"));
+                tmp.First();
+                long PAYLOAD_POSITION = long.Parse(reg.Replace(br.ReadLine(), "$1"));
+                long PAYLOAD_SIZE = long.Parse(reg.Replace(br.ReadLine(), "$1"));
+                long PRELUDE_POSITION = long.Parse(reg.Replace(br.ReadLine(), "$1"));
+                long PRELUDE_SIZE = long.Parse(reg.Replace(br.ReadLine(), "$1"));
+
+                br.BaseStream.Seek(PRELUDE_POSITION, SeekOrigin.Begin);
+                br.DiscardBufferedData();
+
+                string tt = @"{""D:\\snapshot";
+                tmp = br.BaseStream.ScanAOB(Encoding.ASCII.GetBytes(tt));
+                tmp.First();
+                br.BaseStream.Seek(-tt.Length, SeekOrigin.Current);
+                br.DiscardBufferedData();
+                string pre = br.ReadToEnd();
+                foreach (Match m in new Regex(@"""([^""]*)"":{(""\d"":\[\d+,\d+],{0,1})+}").Matches(pre))
+                {
+                    JObject obj = (JObject)Newtonsoft.Json.JsonConvert.DeserializeObject("{" + m.Value + "}");
+                    JToken o = obj.First;
+                    string name = o.First.Path.Replace("['", string.Empty).Replace("']", string.Empty).Replace("\\\\", "\\");
+
+                    if (!name.Contains("node_modules"))
+                    {
+                        JToken n = o.First.SelectToken("1");
+                        if (n != null)
+                        {
+                            long pos = n.First.Value<long>();
+                            int size = n.Last.Value<int>();
+
+                            byte[] buff = new byte[size];
+                            br.BaseStream.Seek(PAYLOAD_POSITION + pos, SeekOrigin.Begin);
+                            br.DiscardBufferedData();
+                            br.BaseStream.Read(buff, 0, size);
+
+                            if (!System.IO.Directory.Exists(System.IO.Path.GetDirectoryName(name)))
+                            {
+                                Directory.CreateDirectory(System.IO.Path.GetDirectoryName(name));
+                            }
+
+                            File.WriteAllBytes(name, buff);
+                        }
+                    }
+                }
+
+                Console.WriteLine(PAYLOAD_POSITION);
+            }
+        }
+    }
+}
---
+++ PkgExeExtract/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkgExeExtract
+{
+    public static class Utils
+    {
+        public static IEnumerable<long> ScanAOB(this Stream stream, params byte[] aob)
+        {
+            long position;
+            byte[] buffer = new byte[aob.Length - 1];
+
+            while ((position = stream.Position) < stream.Length)
+            {
+                if (stream.ReadByte() != aob[0]) continue;
+                if (stream.Read(buffer, 0, aob.Length - 1) == 0) continue;
+
+                if (buffer.SequenceEqual(aob.Skip(1)))
+                {
+                    yield return position;
+                }
+            }
+        }
+
+        public static IEnumerable<long> ScanAOB(this Stream stream, params byte?[] aob)
+        {
+            long position;
+            byte[] buffer = new byte[aob.Length - 1];
+
+            while ((position = stream.Position) < stream.Length)
+            {
+                if (stream.ReadByte() != aob[0]) continue;
+                if (stream.Read(buffer, 0, aob.Length - 1) == 0) continue;
+
+                if (buffer.Cast<byte?>().SequenceEqual(aob.Skip(1), new AobComparer()))
+                {
+                    yield return position;
+                }
+            }
+        }
+
+        private class AobComparer : IEqualityComparer<byte?>
+        {
+            public bool Equals(byte? x, byte? y) => x == null || y == null || x == y;
+            public int GetHashCode(byte? obj) => obj?.GetHashCode() ?? 0;
+        }
+    }
+}
